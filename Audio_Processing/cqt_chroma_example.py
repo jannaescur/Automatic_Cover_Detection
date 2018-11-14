158c6b4010fe4741257b300fedc75faeb0bbd347
jannaescur <janna.escur@alu-etsetb.upc.edu>
2018-11-14 15:10:53 +0100

    before merge

--- Audio_Processing/cqt_chroma_example.py
+++ Audio_Processing/cqt_chroma_example.py
@@ -32,7 +32,7 @@ import librosa.display
 
 #######################################################################
 # We'll use a track that has harmonic, melodic, and percussive elements
-y, sr = librosa.load('C:\Users\janna\Downloads\Automatic_Cover_Detection-master\Automatic_Cover_Detection-master\Tests\The_Specials_-_You_re_Wondering_Now-xEPfSWk0Lsw.wav')
+y, sr = librosa.load('C:\Users\janna\Documents\MATT\DSAP\Projecte\Automatic_Cover_Detection-master\Tests\Amy_Winehouse_-_You_re_Wondering_Now-9b3lo5a3iEk.wav')
 
 
 #######################################
